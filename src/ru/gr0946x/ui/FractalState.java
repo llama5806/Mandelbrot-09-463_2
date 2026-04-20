65697dfbdaaf97375988c263a216e3d583e35299
MinibaevR <ruslanplay206@gmail.com>
2026-04-20 15:37:31 +0300

    -Добавил модель для хранения координат

---
+++ src/ru/gr0946x/ui/FractalState.java
@@ -0,0 +1,12 @@
+package ru.gr0946x.ui;
+
+public class FractalState {
+    public final double xMin, xMax, yMin, yMax;
+
+    public FractalState(double xMin, double xMax, double yMin, double yMax) {
+        this.xMin = xMin;
+        this.xMax = xMax;
+        this.yMin = yMin;
+        this.yMax = yMax;
+    }
+}
